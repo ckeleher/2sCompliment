1d89c48400b158178ee4f55f79323ef71ea2f57a
Tyler <tylertagawa@gmail.com>
2016-03-01 18:28:42 -0800

    Added firebase support for the high score database.

--- app/src/main/java/chamberlin/daniel/twoscompliment/MainActivity.java
+++ app/src/main/java/chamberlin/daniel/twoscompliment/MainActivity.java
@@ -3,11 +3,16 @@ package chamberlin.daniel.twoscompliment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.firebase.client.Firebase;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Firebase.setAndroidContext(this);
+        Firebase firebase = new Firebase("https://shining-inferno-9683.firebaseio.com/");
+        firebase.child("Frank").setValue(99999999);
     }
 }
