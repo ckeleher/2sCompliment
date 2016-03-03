a0edbbd8d399b2a7114d431f2aebedf01f1ecbed
bion719 <djchambe@ucsc.edu>
2016-03-03 15:06:41 -0800

    imageButton implimentation for fourbyfour board

--- app/src/main/java/chamberlin/daniel/twoscompliment/MainActivity.java
+++ app/src/main/java/chamberlin/daniel/twoscompliment/MainActivity.java
@@ -1,7 +1,9 @@
 package chamberlin.daniel.twoscompliment;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startGame(View v){
+
+        //switch to different activity
+        Intent intent = new Intent(this, boardActivity.class);
+        startActivity(intent);
     }
 }
